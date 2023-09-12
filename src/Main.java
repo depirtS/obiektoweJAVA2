ffb917c82adaf8f9b0f57a9711f19a9c9e9530d8
depirtS <karol.fol@gmail.com>
2023-09-12 11:28:55 +0200

    Konstruktory, przeciazanie, metoda toString()

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
         System.out.println("imie naszej osoby " + osoba.getImie());
         osoba.setWiek(-20);
         System.out.println("Wiek " + osoba.getWiek());
+
+        Osoba osoba2 = new Osoba("Ola","Starsza",19);
+        System.out.println(osoba2);
         }
     }
--- src/osoba.java
+++ src/osoba.java
@@ -3,6 +3,26 @@ public class Osoba {
     private String nazwisko;
     private int wiek;
 
+    //przeciazenie meetod - metody mogo miec te same nazwy,
+    //ale maja inne argumenty
+    //right click - generate - constructor
+    public Osoba(){
+        this.imie = "Edward";
+        this.nazwisko = "Nowak";
+        this.wiek = 30;
+    }
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
     //right click - generate - getter
     public String getImie() {
         return imie;
@@ -38,6 +58,16 @@ public class Osoba {
         else {
             this.wiek = wiek;
         }
+    }
+    //right click - generate - toString()
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", wiek=" + wiek +
+                '}';
     }
 }
 
