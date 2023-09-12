d1d1803babdc9ecfc43f11437790b5b10eede8cb
depirtS <karol.fol@gmail.com>
2023-09-12 11:10:01 +0200

    klasa z publicznymi danymi osoba

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dziala");
+        Osoba osoba = new Osoba();
+        osoba.imie = "Jan";
+        System.out.println("imie naszej osoby " + osoba.imie);
+        osoba.wiek = -20;
+        System.out.println("Wiek " + osoba.wiek);
+        }
+    }
