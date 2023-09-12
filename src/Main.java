6926f201ef9e6544be88faf8f10b7095fbdc1b6d
depirtS <karol.fol@gmail.com>
2023-09-12 12:19:50 +0200

    Nie wiem, copy paste bo nie dziala git

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dziala");
-        Osoba osoba = new Osoba();
+        System.out.println("Hello world!");
+        /*Osoba osoba = new Osoba();
         osoba.setImie("Jan");
-        System.out.println("imie naszej osoby " + osoba.getImie());
+        System.out.println("Imie naszej osoby "+osoba.getImie());
         osoba.setWiek(-20);
-        System.out.println("Wiek " + osoba.getWiek());
+        System.out.println("Wiek "+osoba.getWiek());
 
         Osoba osoba2 = new Osoba("Ola","Starsza",19);
         System.out.println(osoba2);
+         */
 
-        Uczen uczen1P1 = new Uczen("Emil", "Smalandzki",12);
+        Osoba uczen1P1 = new Uczen("Emil","Smalandzki",12);
         Uczen uczen1P2 = new Uczen("Emilia","Pragmatyczna",9);
-        System.out.println(uczen1P1);
         System.out.println(uczen1P2);
-    }}
+        System.out.println(uczen1P1);
+        Nauczyciel nauczycielSn = new Nauczyciel("Sebastian","Nadwiślański", "język polski","wf");
+        System.out.println(nauczycielSn);
+    }
+}
