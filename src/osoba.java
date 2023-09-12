d88a527514f30ea35965a16635f5cfbd5045a19e
depirtS <karol.fol@gmail.com>
2023-09-12 11:18:15 +0200

    wlasnosci w klasie osoba prywatne metody dostepowe

--- src/osoba.java
+++ src/osoba.java
@@ -1,6 +1,43 @@
 public class Osoba {
-    public String imie;
-    public String nazwisko;
-    public int wiek;
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+    //right click - generate - getter
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    //right click - generate - setter
+    public void setImie(String imie) {
+        if (imie.length()<3)
+            System.out.println("Imie musi miec co najmniej 3 znaki");
+        else {
+            //TODO: sprawdzic czy tylko litery
+            //TODO: pierwsza litera duza pozostale male
+            this.imie = imie;
+        }
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setWiek(int wiek) {
+        if (wiek < 0){
+            this.wiek = 0;
+        }
+        else {
+            this.wiek = wiek;
+        }
+    }
 }
 
